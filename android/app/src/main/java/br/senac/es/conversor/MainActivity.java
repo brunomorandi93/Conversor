1bd93f2aa4e2acec960a98ba468c986de08f1837
Sala 302B <sala302b@ceines.com.br>
2019-08-28 21:50:47 -0300

    Conversor atualizado

--- android/app/src/main/java/br/senac/es/conversor/MainActivity.java
+++ android/app/src/main/java/br/senac/es/conversor/MainActivity.java
@@ -3,12 +3,74 @@ package br.senac.es.conversor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+  // é o nome da classe
+    int alturaEmCentimetros = 0;
+    // declaração da Variavel
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+      // é um metodo protegido só quem herda
+
         setContentView(R.layout.activity_main);
+        // metodo que liga a MainActivity.JAVA no activity_main.xml (TELA)
+
+        final TextView txtMetros = (TextView) findViewById(R.id.txtMetros);
+        // Instancia de um elemento texto para android
+
+        final TextView txtPes = (TextView) findViewById(R.id.txtPes);
+
+        final SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setMax(230);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch (SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                txtPes.setText("Toque em Converter");
+             //frase para Converter
+            }
+
+            @Override
+            public void onProgressChanged (SeekBar seekBar, int progress, boolean fromUser) {
+
+                alturaEmCentimetros = progress;
+                //Declarando uma variavel com o valor igual ao progresso.
+                String texto = formataValorComDoisDigitos (progress / 100.0);
+                texto += "m.";
+                txtMetros.setText(texto);
+
+            }
+        });
+
+        final Button btnConverter = (Button) findViewById(R.id.btnConverter);
+        btnConverter.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double alturaEmPes = alturaEmCentimetros / 30.48;
+                String texto = formataValorComDoisDigitos(alturaEmPes);
+                texto += " pé(s)";
+                txtPes.setText(texto);
+            }
+        });
+    }
+    private String formataValorComDoisDigitos(double valor){
+        return String.format(Locale.FRANCE, "%.2f", valor);
+
+
+
     }
 }
